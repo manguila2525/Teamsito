274a08db0e3c092172eac57f0794a2c6da0eb131
manguila2525 <manguilas2525@gmail.com>
2022-01-20 13:58:10 -0400

    Maikol - 20/1/22

--- src/controllers/videos.controller.js
+++ src/controllers/videos.controller.js
@@ -1,22 +1,46 @@
+const videosModel = require('../models/Videos.models')
 
-
-const getVideos = async(req, res) => {
+const getVideos = async (req, res) => {
   res.send('Get Videos')
 }
 
-const getVideo = async(req, res) =>{
-
+const getVideo = async (req, res) => {
+  try {
+    const { id } = req.params
+    const video = await videosModel.findOne({ _id: id })
+    res.status(200)
+    res.json({
+      video
+    })
+  } catch (err) {
+    res.status(404)
+    res.json({ Error: "Error in getVideo" })
+  }
 }
 
-const createVideo = async(req, res) =>{
-
+const createVideo = async (req, res) => {
+  try {
+    const { titulo, descripcion, duracion } = req.body
+    const video = await videosModel.create({ titulo, descripcion, duracion })
+    res.status(200)
+    res.json({ video })
+  } catch (err) {
+    res.status(500)
+    res.json({ Error: "Error in createVideo" })
+  }
 }
 
-const updateVideo  = async(req, res) =>{
+const updateVideo = async (req, res) => {
+  try {
+    const { id } = req.params
+    const video = await videosModel.findOneAndUpdate({ _id: id },)
 
+  } catch (err) {
+
+  }
 }
 
-const deleteVideo = async(req, res) =>{
+const deleteVideo = async (req, res) => {
 
 }
 
--- src/models/Videos.models.js
+++ src/models/Videos.models.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const videos = new Schema({
   titulo: String,
   descripcion: String,
-  duracion: Number
-},{
-  timestamps:true
+  duracion: Number,
+}, {
+  timestamps: true
 })
 
 module.exports = mongoose.model("videos", videos)
